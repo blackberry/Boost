2275ca42c074b5bf3dc632f02a99b39003bb779c
Clifford Hung <chung@rim.com>
2012-04-11 10:10:55 -0400

    Updates to build script for test files
    - Source env.sh from PlayBook-build directory.
    - Specify test executables to be built as the debug variant, link to shared
    libraries and be multi-threaded.

--- PlayBook-test/build-tests.sh
+++ PlayBook-test/build-tests.sh
@@ -2,6 +2,8 @@
 
 # Compile every library's test programs
 
+. ../PlayBook-build/env.sh
+
 # ensure required BBNDK env variables are set
 : ${QNX_HOST:?"Error: QNX_HOST environment variable is not set."}
 : ${QNX_TARGET:?"Error: QNX_TARGET environment variable is not set."}
@@ -15,18 +17,17 @@ if [ ! -d $PREFIX ] ; then
 fi
 
 BJAM=$BOOST_DIR/bjam
-PYTHON_SRC=/home/chung/dev/product-rim/trunk/tools/python-3.2
 
 pushd $BOOST_DIR/libs
 for TDIR in `cat $TEST_DIR/test.list | grep -v '#'` ; do
     if [ -d $TDIR ] ; then
         pushd $TDIR
         $BJAM -a \
-            include="$PYTHON_SRC/Include:$PYTHON_SRC" \
             --prefix=$PREFIX \
             --user-config=$BOOST_DIR/blackberry-armv7le-config.jam \
             --layout=system toolset=qcc target-os=qnxnto \
-            c++-template-depth=900
+            c++-template-depth=900 \
+            variant=debug link=shared threading=multi
         popd
     fi
 done
