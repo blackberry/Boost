d9d1838685aeb1509e822217c40148a9e788ebe3
Clifford Hung <chung@rim.com>
2012-04-25 11:53:21 -0400

    Move build files for libboost_test to a subdirectory so that directories for other builds at the same level can be added

--- libs/test/build/test/common.mk
+++ libs/test/build/test/common.mk
@@ -7,9 +7,9 @@ include $(QCONFIG)
 # Preset make macros go here
 #####################################
 
-EXTRA_SRCVPATH+=$(PRODUCT_ROOT)/src
+EXTRA_SRCVPATH+=$(PRODUCT_ROOT)/../src
 
-EXTRA_INCVPATH+=$(PRODUCT_ROOT)/../..
+EXTRA_INCVPATH+=$(PRODUCT_ROOT)/../../..
 
 # Use same flags from Jamfile to make a shared library
 CCFLAGS += -DBOOST_TEST_DYN_LINK=1 
@@ -19,10 +19,10 @@ EXCLUDE_OBJS += \
   cpp_main.o \
   test_main.o
 
-include ../../../../rim_preset.mk
+include ../../../../../rim_preset.mk
 
 define PINFO
-PINFO DESCRIPTION=Boost.Testest library
+PINFO DESCRIPTION=Boost.Test library
 endef
 
 NAME=boost_test
@@ -33,4 +33,4 @@ include $(MKFILES_ROOT)/qtargets.mk
 # Post-set make macros go here
 #####################################
 
-include ../../../../rim_postset.mk
+include ../../../../../rim_postset.mk
