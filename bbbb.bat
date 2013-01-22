0a2d5d641b8a45da8a68582e35e941f39fff2cca
Tony Van Eerd <tvaneerd@rim.com>
2013-01-22 10:17:34 -0500

    bb boost build convenience/example script

--- bbbb.bat
+++ bbbb.bat
@@ -1,2 +1,2 @@
 :: run b2 --help for info on the options
-b2 %* --debug-configuration install --prefix=C:\boost\ --user-config=blackberry-config.jam --layout=system toolset=qcc target-os=qnxnto
+b2 %* install --prefix=C:\boost\ --user-config=blackberry-config.jam --layout=system toolset=qcc target-os=qnxnto
