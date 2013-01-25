48fdc09e7e57bd7c3a9c0130c3fbdebe7700a9d1
Peter Sosnowski <psosnowski@rim.com>
2013-01-25 13:42:21 -0500

    Add '--without-python' option to build without python Comment out references to python in config file

--- bbbb.sh
+++ bbbb.sh
@@ -1,2 +1,2 @@
 # run b2 --help for info on the options
-b2 $@ install --prefix=./boost/ --user-config=blackberry-config.jam --layout=system toolset=qcc target-os=qnxnto
+b2 $@ install --prefix=./boost/ --user-config=blackberry-config.jam --layout=system toolset=qcc target-os=qnxnto --without-python
